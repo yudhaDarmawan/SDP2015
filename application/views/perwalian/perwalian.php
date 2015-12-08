ead1162ebbaee1cc8cd89a0f5254fcb4a2f0becf
Stefanie Tanujaya <stefanie.tan95@gmail.com>
2015-12-08 22:23:16 +0700

    Revert "commit"

--- application/views/perwalian/perwalian.php
+++ application/views/perwalian/perwalian.php
@@ -1,4 +1,4 @@
-<?=form_open('perwalian/frs')?>
+<?php echo form_open('perwalian/mahasiswa');?>
 <!--CONTENT-->
 	
 	<h2 class="text-center"><?php echo $nowSemester;?></h2>
@@ -133,4 +133,4 @@
 	$(document).ready(function(){
 	});
 </script>
-<?=form_close()?>
+<?php echo form_close(); ?>
